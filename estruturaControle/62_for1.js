85c1d8ed04cec7dbcbd668888e637c97d538040f
rafaelthosi <thhrafael@outlook.com>
2021-01-25 20:57:03 -0300

    conteúdo 62_for1

---
+++ estruturaControle/62_for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while(contador <= 10) {
+    console.log(`Contador: ${contador}`)
+    contador++
+}
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`i: ${i}`)
+}
+
+const notas = [6.7, 4.5, 8.4, 9.9, 32]
+for(i = 0; i < notas.length; i++) {
+    console.log(`Notas: ${notas[i]}`)
+}
