773b2aeccb534957ddc36fafb23f33512ee340ff
rafaelthosi <thhrafael@outlook.com>
2021-04-14 15:02:02 -0300

    arrumando nomes; estruturaControle finalizado

---
+++ estruturaControle/64_for2.js
@@ -0,0 +1,18 @@
+const notas = [6.7, 4.5, 8.4, 9.9, 32]
+
+for(let i in notas) {
+    //console.log(i, notas[i])
+}
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: "Silva",
+    idade: 29,
+    peso: 64
+}
+
+for(let atributo in pessoa) { //se não colocar let o atributo sai do bloco com seu ultimo valor(peso)
+    if (atributo == "nome") {
+        console.log(pessoa[atributo])
+    }
+}
