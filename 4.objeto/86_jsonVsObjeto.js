fd6fc779f4f64dbb1fa0e6f947d7a18c5f3efc43
rafaelthosi <thhrafael@outlook.com>
2021-06-14 20:07:21 -0300

    correção

--- 4.objeto/86_jsonVsObjeto.js
+++ 4.objeto/86_jsonVsObjeto.js
@@ -3,5 +3,6 @@ console.log(JSON.stringify(obj)) // transforma em json
 
 // console.log(JSON.parse("{ a: 1, b: 2, c: 3 }")) Erro
 // console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }")) Erro
-console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }')) // transforma um json em string
-console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }')) // transforma um json em string
+console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }')) // transforma um json em objeto
+console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }')) // transforma um json em objeto
+const teste = JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }')
